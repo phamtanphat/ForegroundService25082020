280c2fdd411d59494a77a4bf03e19864918386b7
phatdroid94 <phatdroid94@gmail.com>
2020-10-27 21:09:19 +0700

    foreground service

--- app/src/main/java/com/example/foregroundservice25082020/MainActivity.java
+++ app/src/main/java/com/example/foregroundservice25082020/MainActivity.java
@@ -1,22 +1,32 @@
 package com.example.foregroundservice25082020;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
+    Button mBtnStart,mBtnStop;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mBtnStart = findViewById(R.id.buttonStartForeground);
+        mBtnStop = findViewById(R.id.buttonStopForeground);
+
+        mBtnStart.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this,MyService.class);
+                ContextCompat.startForegroundService(MainActivity.this,intent);
+            }
+        });
     }
 
-    @Override
-    protected void onStart() {
-        super.onStart();
-        Intent intent = new Intent(this , MyService.class);
-        startService(intent);
-    }
+
 }
--- app/src/main/java/com/example/foregroundservice25082020/MyService.java
+++ app/src/main/java/com/example/foregroundservice25082020/MyService.java
@@ -1,13 +1,25 @@
 package com.example.foregroundservice25082020;
 
+import android.app.Notification;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
 import android.app.Service;
+import android.content.Context;
 import android.content.Intent;
+import android.graphics.BitmapFactory;
+import android.graphics.Color;
+import android.os.Build;
 import android.os.IBinder;
 import android.util.Log;
+import android.widget.Toast;
 
 import androidx.annotation.Nullable;
+import androidx.core.app.NotificationCompat;
 
 public class MyService extends Service {
+    String MY_CHANNEL = "MY_CHANNEL";
+    NotificationManager mNotificationManager;
+    NotificationCompat.Builder mNotification;
     @Nullable
     @Override
     public IBinder onBind(Intent intent) {
@@ -17,12 +29,46 @@ public class MyService extends Service {
     @Override
     public void onCreate() {
         super.onCreate();
-        Log.d("BBB","onCreate");
+        mNotificationManager = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
+        mNotification = createNotification(this);
+        startForeground(1,mNotification.build());
     }
 
     @Override
     public int onStartCommand(Intent intent, int flags, int startId) {
         Log.d("BBB","onStartCommand");
-        return super.onStartCommand(intent, flags, startId);
+        Toast.makeText(this, "onStartCommand", Toast.LENGTH_SHORT).show();
+        return START_STICKY;
     }
+    private NotificationCompat.Builder createNotification(Context context){
+        NotificationCompat.Builder notification =
+                new NotificationCompat.Builder(context, MY_CHANNEL)
+                        .setContentTitle("Ban cap nhat moi")
+                        .setContentText("Phien ban app 15.0")
+                        .setShowWhen(true)
+                        .setSmallIcon(R.drawable.ic_launcher_background)
+                        .setLargeIcon(
+                                BitmapFactory.decodeResource(
+                                        getResources(),
+                                        R.drawable.icon_jetpack
+                                ))
+                        .setStyle(new NotificationCompat.BigPictureStyle().bigPicture(BitmapFactory.decodeResource(
+                                getResources(),
+                                R.drawable.icon_jetpack
+                        )))
+                        .setOngoing(true);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            NotificationChannel notificationChannel =
+                    new NotificationChannel(
+                            MY_CHANNEL,
+                            "CHANNEL",
+                            NotificationManager.IMPORTANCE_HIGH);
+            notificationChannel.enableLights(true);
+            notificationChannel.setLightColor(Color.RED);
+            notificationChannel.enableVibration(true);
+            mNotificationManager.createNotificationChannel(notificationChannel);
+        }
+        return notification;
+    }
+
 }
